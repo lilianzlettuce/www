a1459c0aae55e59dd854382a9827dce725ec837b
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2025-06-29 23:54:15 -0500

    edited experiments page

--- src/app/experiments/page.tsx
+++ src/app/experiments/page.tsx
@@ -4,8 +4,9 @@ export default async function ExperimentsPage() {
 
   return (
     <div className="min-h-screen bg-background p-20">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-20">
         <CirclesCanvas />
+        <canvas id="starCanvas" className="relative xl:-ml-[58px] lg:-ml-[50px] md:-ml-[40px] sm:left-0 left-0"></canvas>
       </div>
     </div>
   );
