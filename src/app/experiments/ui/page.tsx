88c9af8e97e1d3272d92aa907f9ecbd071a75c1f
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2025-08-01 15:49:59 -0500

    styled window components

--- src/app/experiments/ui/page.tsx
+++ src/app/experiments/ui/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/Buttons";
 import { DraggableWindow } from "@/components/DraggableWindow";
 import Draggable from "@/components/Draggable";
 import { ArrowLeftIcon, GithubIcon, ExternalLinkIcon, XIcon, SwatchIcon, MoonIcon, SunIcon, EyeIcon, ExpandIcon, ExpandIcon2, MinimizeIcon } from "@/components/Icons";
+import Sprite from "@/components/Sprite";
 
 export default function UIExperimentsPage() {
   return (
@@ -10,7 +11,7 @@ export default function UIExperimentsPage() {
         <NavBarGrid />
         <NavBarBasic />
         <NavBarFloating />
-        <div className="h-screen flex flex-col items-center justify-center">
+        <div className="h-screen mt-[400px] p-48 flex flex-col items-center justify-center">
             <h1 className="font-psygen uppercase text-[150px] font-bold">UI Experiments</h1>
             <h1 className="font-archivo-black uppercase text-8xl font-bold">UI Experiments</h1>
             
@@ -20,20 +21,35 @@ export default function UIExperimentsPage() {
             </p>
             <Button text="Hello" />
 
-            <div className="w-screen h-screen b-green-300 p-4 flex flex-row gap-4">
+            <div className="w-screen min-h-screen h-fit b-green-300 p-4 flex flex-row flex-wrap gap-4">
               <div className="relative">
                 <DraggableWindow />
               </div>
               <Draggable>
                 <div className="z-100 relative w-[300px] h-[300px] flex flex-col bg-background text-foreground border border-foreground shadow-lg">
                   <div className="handle select-none cursor-grab px-1 py-0 flex flex-row justify-between items-center bg-foreground text-background text-sm font-tiny5 uppercase">
-                    <div className="flex flex-row gap-2">
-                      Accessing...
+                    <div className="flex flex-row items-center gap-2">
+                      <div className="mix-blend-difference">
+                        <Sprite
+                          id="eye-icon"
+                          spriteSize={16}
+                          backgroundImage="/img/sprite/eye-open.png"
+                          steps={1}
+                          duration={1}
+                          onHover={true}
+                          hoverSteps={2}
+                          hoverDuration={0.3}
+                          hoverBackgroundImage="/img/sprite/goose-walk.png"
+                          style={{ scale: 1.1 }}
+                        />
+                      </div>
+                      <div>
+                        Accessing...
+                      </div>
                     </div>
                     <div className="flex flex-row items-center justify-between gap-0">
-                      <ExpandIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
+                    <MinimizeIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
                       <ExpandIcon2 className="w-4.5 h-4.5" strokeWidth={2.5} />
-                      <MinimizeIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
                       <XIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
                     </div>
                   </div>
@@ -46,8 +62,31 @@ export default function UIExperimentsPage() {
               </Draggable>
               <Draggable>
                 <div className="z-100 relative w-[300px] h-fit flex flex-col bg-background text-foreground border border-foreground shadow-lg">
-                  <div className="handle select-none cursor-grab px-1 py-0 text-foreground text-sm font-tiny5 uppercase border-b-1 border-foreground">
-                    Greetings
+                <div className="handle select-none cursor-grab px-1 py-0 flex flex-row justify-between items-center border-b-1 border-foreground text-sm font-tiny5 uppercase">
+                    <div className="flex flex-row items-center gap-2">
+                      <div className="mix-blend-difference">
+                        <Sprite
+                          id="eye-icon"
+                          spriteSize={16}
+                          backgroundImage="/img/sprite/eye-open.png"
+                          steps={1}
+                          duration={1}
+                          onHover={true}
+                          hoverSteps={2}
+                          hoverDuration={0.3}
+                          hoverBackgroundImage="/img/sprite/goose-walk.png"
+                          style={{ scale: "1.05 1.05" }}
+                        />
+                      </div>
+                      <div>
+                        Greetings
+                      </div>
+                    </div>
+                    <div className="flex flex-row items-center justify-between gap-0">
+                      <MinimizeIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
+                      <ExpandIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
+                      <XIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
+                    </div>
                   </div>
                   <div className="flex-1 flex flex-col items-center justify-center">
                     <div className="w-full h-full p-4 font-roboto-mono text-xs">
@@ -83,6 +122,82 @@ export default function UIExperimentsPage() {
                         <SwatchIcon className="w-4.5 h-4.5" strokeWidth={1.5} />
                         <EyeIcon className="w-4.5 h-4.5" strokeWidth={1.5} />
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </Draggable>
+
+              <Draggable>
+                <div className="z-100 relative w-[300px] h-fit flex flex-col bg-background text-foreground border border-foreground shadow-lg">
+                  <div className="handle select-none cursor-grab px-1 py-0 flex flex-row justify-between items-center border-b-1 border-foreground text-sm font-tiny5 uppercase">
+                    <div className="flex flex-row items-center gap-2">
+                      <div className="mix-blend-difference">
+                        <Sprite
+                          id="eye-icon"
+                          spriteSize={16}
+                          backgroundImage="/img/sprite/eye-open.png"
+                          steps={1}
+                          duration={1}
+                          onHover={true}
+                          hoverSteps={2}
+                          hoverDuration={0.3}
+                          hoverBackgroundImage="/img/sprite/goose-walk.png"
+                          style={{ scale: "1.05 1.05" }}
+                        />
+                      </div>
+                      <div>
+                        Alert!!!
+                      </div>
+                    </div>
+                    <div className="flex flex-row items-center justify-between gap-0">
+                      <XIcon className="w-4.5 h-4.5" strokeWidth={2.5} />
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col items-center justify-center">
+                    <div className="w-full h-full p-4 font-roboto-mono text-xs">
+                      This is the window content.
+                    </div>
+                  </div>
+                </div>
+              </Draggable>
+
+              <Draggable>
+                <div className="z-100 relative w-[300px] h-fit p-1 flex flex-col bg-background text-foreground border-2 border-foreground shadow-lg">
+                  <div className="handle select-none cursor-grab px-1 pb-1 flex flex-row justify-between items-center">
+                    <div className="flex flex-row items-center gap-2">
+                      <div className="mix-blend-difference">
+                        <Sprite
+                          id="eye-icon"
+                          spriteSize={16}
+                          backgroundImage="/img/sprite/eye-open.png"
+                          steps={1}
+                          duration={1}
+                          onHover={true}
+                          hoverSteps={2}
+                          hoverDuration={0.3}
+                          hoverBackgroundImage="/img/sprite/goose-walk.png"
+                          style={{ scale: "1.2 1.2" }}
+                        />
+                      </div>
+                    </div>
+                    <div className="text-md font-tiny5 uppercase">
+                      Greetings
+                    </div>
+                    <div className="flex flex-row items-center justify-between gap-2">
+                      <div className="w-4.5 h-4.5 box-border border-2 border-foreground flex items-center justify-center">
+                        <MinimizeIcon className="scale-125" strokeWidth={2.5} />
+                      </div>
+                      <div className="w-4.5 h-4.5 box-border border-2 border-foreground flex items-center justify-center">
+                        <ExpandIcon className="scale-125" strokeWidth={2.5} />
+                      </div>
+                      <div className="w-4.5 h-4.5 box-border border-2 border-foreground flex items-center justify-center">
+                        <XIcon className="scale-125" strokeWidth={2.5} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col items-center justify-center border-2 border-foreground">
+                    <div className="w-full h-full p-4 font-roboto-mono text-xs">
+                      This is the window content.
                     </div>
                   </div>
                 </div>
